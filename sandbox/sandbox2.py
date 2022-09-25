3f0d65210a55cb0a8e1c6b88b6e03363aa0b17d8
giovanni <ionmocanu2266@gmail.com>
2022-09-25 11:55:18 +0300

    a

--- sandbox/sandbox2.py
+++ sandbox/sandbox2.py
@@ -1,17 +1,4 @@
-def find_var_name(var_name):
-    for objname, oid in globals().items():
-        if oid is var_name:
-            return True
-    
-    return False
+from pyjokes.pyjokes import get_joke
 
-class Class1:
 
-    def __init__(self,atr1):
-        self.atr1 = atr1
-        self.print_name()
-        
-    def print_name(self):
-        print(find_var_name(str(self)))
-
-test = Class1("abc")
+print(get_joke("it"))
