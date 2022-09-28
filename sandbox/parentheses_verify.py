e4055911e5d6eb1f3d96f17b73dcdae3fe4fcf52
programare2019colegiu <programare2019colegiu@gmail.com>
2022-09-28 08:12:42 +0300

    a

--- sandbox/parentheses_verify.py
+++ sandbox/parentheses_verify.py
@@ -1,38 +1,13 @@
-# cate inchise atatea si deshise
-# 
-
-
-
-'''class Paranteze:
-    paranteze_deschise = ["(","[","{"]
-    paranteze_inchise = [")","]","}"]
-    def __init__(self,string):
-        self.string = string
-        print("parantezele: ",string," este ",self.is_valid())
-
-    def is_valid(self):
-        s = self.string
-        for i in range(len(s)):
-            if s[i] in  self.paranteze_deschise:
-                return "valabil"
-            else:
-                return "nevalabil"'''
-
-
-
-
-
-
-#p = Paranteze("()")
-class py_solution:
-   def is_valid_parenthese(self, str1):
-        stack, pchar = [], {"(": ")", "{": "}", "[": "]"}
-        for parenthese in str1:
-            print(stack)
-            if parenthese in pchar:
-                stack.append(parenthese)
-            elif len(stack) == 0 or pchar[stack.pop()] != parenthese:
-                return False
-        return len(stack) == 0
-#stack = [ (     ]
-print(py_solution().is_valid_parenthese("()()()()()()()()()()()())()()"))
+def generateParenthesis(n = 1,str="",openS=0,closeS=0):
+    if openS == closeS == n:
+        print(str)
+        return
+    if openS < n:
+        str+="("
+        generateParenthesis(n,str,openS+1,closeS)
+        str=str[0:-1]
+    if closeS < openS:
+        str+=")"
+        generateParenthesis(n,str,openS,closeS+1)
+        str=str[0:-1]
+generateParenthesis()
