6b1283ed044afe47db75bd6fd5633957191c392f
giovanni <ionmocanu2266@gmail.com>
2022-09-28 21:58:51 +0300

    a

--- polls/admin.py
+++ polls/admin.py
@@ -2,6 +2,7 @@ from tkinter.tix import InputOnly
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question
+from .models import Question,Choice
 
 admin.site.register(Question)
+admin.site.register(Choice)
